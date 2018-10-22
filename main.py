957df4cf2cc8d6104044d84a17afe581d95f3683
tobimori <tobi.mori92@gmail.com>
2018-10-22 14:40:26 +0200

    Working *dab* (for Windows)

--- main.py
+++ main.py
@@ -1,78 +1,38 @@
+# Unsplash Wallpaper
+version = "0.1"
+# last edited @ 22.10.2018
+# by tobimori
+
 import platform
-import sys
 import os
 import urllib.request
 import time
-import dbus
-
-def get_desktop_environment():
-    # From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
-    # and http://ubuntuforums.org/showthread.php?t=652320
-    # and http://ubuntuforums.org/showthread.php?t=652320
-    # and http://ubuntuforums.org/showthread.php?t=1139057
-    if sys.platform in ["win32", "cygwin"]:
-        return "windows"
-    elif sys.platform == "darwin":
-        return "mac"
-    else:  # Most likely either a POSIX system or something not much common
-        desktop_session = os.environ.get("DESKTOP_SESSION")
-        if desktop_session is not None:  # easier to match if we doesn't have  to deal with caracter cases
-            desktop_session = desktop_session.lower()
-            if desktop_session in ["gnome", "unity", "cinnamon", "mate", "xfce4", "lxde", "fluxbox",
-                                   "blackbox", "openbox", "icewm", "jwm", "afterstep", "trinity", "kde"]:
-                return desktop_session
-            ## Special cases ##
-            # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
-            # There is no guarantee that they will not do the same with the other desktop environments.
-            elif "xfce" in desktop_session or desktop_session.startswith("xubuntu"):
-                return "xfce4"
-            elif desktop_session.startswith("ubuntu"):
-                return "unity"
-            elif desktop_session.startswith("lubuntu"):
-                return "lxde"
-            elif desktop_session.startswith("kubuntu"):
-                return "kde"
-            elif desktop_session.startswith("razor"):  # e.g. razorkwin
-                return "razor-qt"
-            elif desktop_session.startswith("wmaker"):  # e.g. wmaker-common
-                return "windowmaker"
-        if os.environ.get('KDE_FULL_SESSION') == 'true':
-            return "kde"
-        elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-            if not "deprecated" in os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-                return "gnome2"
-        # From http://ubuntuforums.org/showthread.php?t=652320
-        elif self.is_running("xfce-mcs-manage"):
-            return "xfce4"
-        elif self.is_running("ksmserver"):
-            return "kde"
-    return "unknown"
+import ctypes
 
 def get_image():
-    #dirvar = os.path.join(".unsplash")
-    #if not os.path.exists(dirvar):
-    #    os.makedirs(dirvar)
-    filename = str(time.time()) + ".jpg"
-    urllib.request.urlretrieve("https://source.unsplash.com/daily",  filename)
-    return filename
-
-version = "0.1"
+    directory = os.getcwd() + "/.unsplash"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print("Found no temporary directory. Creating one.")
+    filepath = directory + "/" + str(time.time()) + ".jpg"
+    try:
+        urllib.request.urlretrieve("https://source.unsplash.com/random", filepath)
+        print(f"We downloaded that image to {filepath}")
+        return filepath
+    except:
+        print("Something failed while downloading the image.")
+        exit()
 
 print(f"Unsplash Wallpaper v{version}\n")
-de = get_desktop_environment()
 osvar = platform.system()
-print(f"You are using {osvar} with {de.upper()} installed.")
 
-
-if osvar == "Linux":
-    if de == "kde":
-        filename = get_image()
-    #    currdir = os.getcwd()
-    #    bus = dbus.SessionBus()
-    #    remote_object = bus.get_object("org.kde.plasmashell","/PlasmaShell")
-    #    remote_object.evaluateScript('var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file://' + currdir + '/pictures/' + filename + '.jpg' + '")}', dbus_interface = "org.kde.PlasmaShell")
-
-    else:
-        print("Sorry, currently only supporting KDE.")
+if osvar == "Windows":
+    print("We found that you're using Windows. Great. Let's do this!")
+    filepath_absolute = get_image()
+    try:
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath_absolute, 0)
+    except:
+        print("We couldn't set your wallpaper.")
+        exit()
 else:
-    print("Sorry, only supporting some limited DEs on Linux right now. Feel free to fork and add support ;)")
+    print("Sorry, only supporting Windows right now. Feel free to fork and add support ;)")
